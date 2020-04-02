e81913343a98ed302861c5e3266ccc3fa8362e17
Safar1997 <kurbonov_safar@bk.ru>
2020-04-02 21:05:40 +0300

    Updates

---
+++ precache-manifest.0f7902fb11a831b2dcd13543f30f2658.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe591d858dc0a7c91da6ea0043feea24",
+    "url": "/aviasales/index.html"
+  },
+  {
+    "revision": "4aedcb59519944dfd0df",
+    "url": "/aviasales/static/js/2.46267d65.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aviasales/static/js/2.46267d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62461eed23547df71fea",
+    "url": "/aviasales/static/js/main.d8cce6a2.chunk.js"
+  },
+  {
+    "revision": "1de7a9a30ac95f7919b3",
+    "url": "/aviasales/static/js/runtime-main.03675960.js"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/aviasales/static/media/logo.0c089234.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aviasales/precache-manifest.4c541e099d1e50c07ce9b3cb0754ad6d.js"
+  "/aviasales/precache-manifest.0f7902fb11a831b2dcd13543f30f2658.js"
 );
 
 self.addEventListener('message', (event) => {
